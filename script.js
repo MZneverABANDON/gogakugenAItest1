9f43e71ec9e230858e2c555e604c224b53e638eb
MZneverABANDON <132475070+MZneverABANDON@users.noreply.github.com>
2023-08-31 14:53:07 +0900

    script.js

--- script.js
+++ script.js
@@ -7,39 +7,6 @@ function updateDateTime() {
     document.getElementById('time').textContent = time;
 }
 
-let timerInterval = null;
-let elapsedTime = 0;
-
-function startStopTimer() {
-    if (timerInterval) {
-        clearInterval(timerInterval);
-        timerInterval = null;
-        document.getElementById('startStop').textContent = 'Start';
-    } else {
-        timerInterval = setInterval(() => {
-            elapsedTime += 1;
-            const hours = Math.floor(elapsedTime / 3600);
-            const minutes = Math.floor((elapsedTime % 3600) / 60);
-            const seconds = elapsedTime % 60;
-            document.getElementById('timerDisplay').textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        }, 1000);
-        document.getElementById('startStop').textContent = 'Stop';
-    }
-}
-
-function resetTimer() {
-    if (timerInterval) {
-        clearInterval(timerInterval);
-        timerInterval = null;
-    }
-    elapsedTime = 0;
-    document.getElementById('timerDisplay').textContent = '00:00:00';
-    document.getElementById('startStop').textContent = 'Start';
-}
-
-document.getElementById('startStop').addEventListener('click', startStopTimer);
-document.getElementById('reset').addEventListener('click', resetTimer);
-
 // 初始化日期时间
 updateDateTime();
 
